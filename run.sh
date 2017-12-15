70acaa0e77e414b4c7d27c17849eb2638982a3f8
Andrey Sizov <andrey.sizov@jetbrains.com>
2017-12-15 15:52:59 +0300

    fix

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/ash
 DATE=`date +%Y%m%d-%H%M%S`
 
 mysqldump -h ${DATABASE_HOST} -u ${DATABASE_USERNAME} --password=${DATABASE_PASSWORD} ${DATABASE_NAME} | gzip > /${DATABASE_NAME}.${DATE}.sql.gz 
